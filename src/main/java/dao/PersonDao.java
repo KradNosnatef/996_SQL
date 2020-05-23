ba0edbe719b13d194ecc00d0efa5a52edd8ec96a
chivier <chivier.humber@gmail.com>
2020-05-23 17:10:46 +0800

    Finish PersonDao. queryStudentPerson = give student id, fetch person information queryTeacherPerson = give teacher id, fetch person information updatePerson = update person information.

--- src/main/java/dao/PersonDao.java
+++ src/main/java/dao/PersonDao.java
@@ -6,28 +6,105 @@ import utils.UnitTestSwitch;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PersonDao {
     public Person queryStudentPerson(String id) {
-        Connection connection;
+        Connection conn;
         if (UnitTestSwitch.SWITCH)
-            connection = DButils.getConnectionUnitTest();
+            conn = DButils.getConnectionUnitTest();
         else
-            connection = DButils.getConnection();
+            conn = DButils.getConnection();
 
         Person person_element = new Person();
+
+        String get_id_card_number_sql = "SELECT * FROM Student WHERE Student_ID = ?;";
+        String id_card_number;
+        String sql = "SELECT * FROM Person WHERE Person_ID_Card_Number=?;";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(get_id_card_number_sql);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                rs.close();
+                ps.close();
+                return null;
+            }
+            id_card_number = rs.getString("Person_ID_Card_Number");
+            rs.close();
+            ps.close();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, id_card_number);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                person_element.set_id_card_number(rs.getString("ID_Card_Number"));
+                person_element.set_card_type(rs.getBoolean("ID_Card_Type"));
+                person_element.set_name(rs.getString("Name"));
+                person_element.set_gender(rs.getBoolean("Gender"));
+                person_element.set_birthdate(rs.getString("Birth"));
+                person_element.set_nationnality(rs.getString("Nationality"));
+                person_element.set_address(rs.getString("Address"));
+                person_element.set_address_postal_code("Address_Postal_Code");
+                person_element.set_address_phone_number("Address_Phone_Number");
+            }
+            ps.close();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        } finally {
+            DButils.closeConnection(conn);
+        }
+
         return person_element;
     }
 
     public Person queryTeacherPerson(String id) {
-        Connection connection;
+        Connection conn;
         if (UnitTestSwitch.SWITCH)
-            connection = DButils.getConnectionUnitTest();
+            conn = DButils.getConnectionUnitTest();
         else
-            connection = DButils.getConnection();
+            conn = DButils.getConnection();
 
         Person person_element = new Person();
+
+        String get_id_card_number_sql = "SELECT * FROM Teacher WHERE Teacher_ID = ?;";
+        String id_card_number;
+        String sql = "SELECT * FROM Person WHERE Person_ID_Card_Number=?;";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(get_id_card_number_sql);
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.next()) {
+                rs.close();
+                ps.close();
+                return null;
+            }
+            id_card_number = rs.getString("Person_ID_Card_Number");
+            rs.close();
+            ps.close();
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, id_card_number);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                person_element.set_id_card_number(rs.getString("ID_Card_Number"));
+                person_element.set_card_type(rs.getBoolean("ID_Card_Type"));
+                person_element.set_name(rs.getString("Name"));
+                person_element.set_gender(rs.getBoolean("Gender"));
+                person_element.set_birthdate(rs.getString("Birth"));
+                person_element.set_nationnality(rs.getString("Nationality"));
+                person_element.set_address(rs.getString("Address"));
+                person_element.set_address_postal_code("Address_Postal_Code");
+                person_element.set_address_phone_number("Address_Phone_Number");
+            }
+            ps.close();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        } finally {
+            DButils.closeConnection(conn);
+        }
+
         return person_element;
     }
 
